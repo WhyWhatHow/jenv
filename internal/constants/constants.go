71940b2acad874e4166788d1ceefa0ae9c12bfe7
whywhathow <whywhathow.fun@gmail.com>
2025-03-09 23:18:00 +0800

    refactor(internal/constants): Add constant definitions

---
+++ internal/constants/constants.go
@@ -0,0 +1,15 @@
+package constants
+
+const (
+	// Windows 系统环境变量路径
+	ENV_SYSTEM_PATH = `SYSTEM\CurrentControlSet\Control\Session Manager\Environment`
+	ENV_USER_PATH   = `Environment`
+
+	// 默认配置路径
+	DEFAULT_CONFIG_FILE = "config.json"
+	DEFAULT_FOLDER      = ".jdks"
+	DEFAULT_BACKUP_FILE = "backup.json"
+
+	// 默认符号链接路径
+	DEFAULT_SYMLINK_PATH = "C:\\Java\\JAVA_HOME"
+)
